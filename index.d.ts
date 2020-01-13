fa1ffd1dbc0dee55b77578a965afd88ada55ca00
Rafael Santos <rafaelnsantos1@gmail.com>
2020-01-13 17:14:49 -0300

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import { Tedis } from 'tedis'
 import { Repository } from 'graphql-api-scripts';
 
-export interface RedisRepository extends Tedis, Repository {
+export interface RedisRepositor extends Tedis, Repository {
 }
 
-export function RedisRepository (folder: string, options: mongoose.ConnectionOptions): RedisRepository
+export function RedisRepository (): RedisRepositor
